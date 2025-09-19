ea3d5eb79775f8716089234baceff6378bf16f13
muradovnamusa-ux <muradovnamusa@gmail.com>
2025-09-19 16:18:43 +0300

    coment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import './App.css';
 
 const App = () => {
   return (
    <>
-   <h1>Assalomu aleykum</h1>
+   <div className="ota">
+    <h1>Logo</h1>
+    <h1>navbar</h1>
+    <h1>navbar</h1>
+    <h1>navbar</h1>
+    <h1>navbar</h1>
+    <h1>navbar</h1>
+   </div>
+
+    <div className="ota2">
+      <h1>
+        1-section
+      </h1>
+    </div>
+
+    <div className="ota3">
+      <div className="box">
+        Left
+      </div>
+      <div className="box">Right</div>
+    </div>
+
+    <div className="ota">
+      <h1>Logo</h1>
+    <h1>navbar</h1>
+    <h1>navbar</h1>
+    <h1>navbar</h1>
+    <h1>navbar</h1>
+    <h1>navbar</h1>
+    </div>
+    
    </>
   )
 }
